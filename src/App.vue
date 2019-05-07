9e565190d389366ddda65f40b739319ee263bc82
pingfei <pingfei1224@163.com>
2019-05-07 15:15:11 +0800

    address scroll

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,32 @@ export default {
 </script>
 
 <style lang='scss'>
+html, body, p, h1, h2, h3, h4, h5, h6, ol, ul, li, dl, dt, dd {
+  margin: 0;
+  padding: 0;
+}
+ol, ul, li {
+  list-style: none;
+}
+input {
+  outline: 0;
+  border: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
+}
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.flex-column {
+  flex-direction: column;
 }
 </style>
--- src/page/address/address.vue
+++ src/page/address/address.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="address">
-    <div @click="postMsg">address</div>
+  <div class="address flex flex-column">
+    <div class="top flex flex-column">
+      <div class="title">
+        <div class="back-btn">back</div>
+        <div class="title">城市选择</div>
+      </div>
+      <div class="search flex">
+        <div class="search-input flex">
+          <i class="icon">123</i>
+          <input type="text" placeholder="请输入地址">
+        </div>
+      </div>
+    </div>
+    <div class="city-list-wrapper"  @scroll="handleScroll" ref="scroll">
+      <div class="city-list">
+        <div class="cities" v-for="(cities, index) in cityList" :key="index">
+          <div class="alphabet" :id="cities.idx">{{cities.idx}}</div>
+          <div class="city" v-for="(city, i) in cities.cities" :key="i">{{city.name}}</div>
+        </div>
+      </div>
+      <div class="alphabet-list flex flex-column">
+        <div :href="'#' + item" v-for="(item, index) in alphabet" :key="index" @click="clickAlphabet(item)">{{item}}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import {getAddress, postAddress} from '../../axios/api.js'
 export default {
   data () {
-    return {}
+    return {
+      cityList: [],
+      alphabet: [],
+      scrollTop: 0
+    }
   },
   beforeMount () {
     getAddress()
       .then(res => {
+        this.cityList = res.cityList
+        this.alphabet = res.alphabet
         console.log(res)
       })
       .catch(err => {
@@ -22,9 +50,118 @@ export default {
   methods: {
     postMsg () {
       postAddress({x: 1, y: 2})
+    },
+    clickAlphabet (alphabet) {
+      let dom = document.getElementById(alphabet)
+      let offsetTop = dom.offsetTop
+      document.querySelector('.city-list-wrapper').scrollTo({
+        top: offsetTop,
+        behavior: 'instant'
+      })
+      
+      // 语法
+      // window.scrollTo(x-coord,y-coord )
+
+      // window.scrollTo(options)
+      // 参数节
+      // x-coord 是文档中的横轴坐标。
+      // y-coord 是文档中的纵轴坐标。
+      // options 是一个包含三个属性的对象:
+      // top 等同于  y-coord
+      // left 等同于  x-coord
+      // behavior  类型String,表示滚动行为,支持参数 smooth(平滑滚动),instant(瞬间滚动),默认值auto,实测效果等同于instant
+      // dom.scrollTop = offsetTop
+      console.log(document.body)
+      console.log(dom)
+      console.log(alphabet)
+      console.log(offsetTop)
+    },
+    handleScroll ($event) {
+      // console.log($event)
+      // console.log(1)
+      // this.scrollTop = this.$refs.content.scrollTop
     }
   }
 }
 </script>
 <style scoped lang="scss">
+.address {
+  width: 100%;
+  height: 100vh;
+}
+.top {
+  color: #fff;
+  font-size: 1.25rem;
+  background: #0089FF;
+  > div {
+    height: 50px;
+    line-height: 50px;
+  }
+  > .title {
+    width: 100%;
+    text-align: center;
+    border-bottom: 1px solid #fff;
+    .back-btn {
+      position: absolute;
+      left: 0;
+    }
+    .title {
+      width: 100%;
+    }
+  }
+  > .search {
+    .search-input {
+      width: 325px;
+      height: 30px;
+      overflow: hidden;
+      margin: auto;
+      padding: 0 10px;
+      color: #ddd;
+      text-align: center;
+      border-radius: 15px;
+      background: #fff;
+      box-sizing: border-box;
+      > input {
+        flex-grow: 1;
+        height: 30px;
+        font-size: 14px;
+      }
+    }
+  }
+}
+.city-list-wrapper {
+  width: 100%;
+  // height: calc(100% - 100px);
+  overflow-y: scroll;
+  position: relative;
+  .city-list {
+    .cities {
+      > div {
+        height: 50px;
+        line-height: 50px;
+        border-bottom: 1px solid #E9E9E9;
+      }
+      .alphabet {
+        padding-left: 10px;
+        background: #F5F5F5;
+      }
+      .city {
+        margin-left: 10px;
+      }
+    }
+  }
+  .alphabet-list {
+    position: fixed;
+    width: 15px;
+    right: 10px;
+    top: 100px;
+    bottom: 0;
+    font-size: 20px;
+    > div {
+      flex-grow: 1;
+      width: 20px;
+      text-align: center;
+    }
+  }
+}
 </style>
